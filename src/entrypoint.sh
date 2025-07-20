8e30534deb84c2797a15e5966c53add63f952638
Alexandru Gojinetchi <MrFail@users.noreply.github.com>
2025-07-21 02:27:04 +0300

    feat(structure): Created alpha version

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update && sudo apt-get install -y sshpass
-
 # Run rsync and capture exit code
-if sshpass -p "$SSH_PASSWORD" scp -r -P "$SSH_PORT" -o StrictHostKeyChecking=no "$SOURCE_PATH" "$SSH_USER@$SSH_HOST:$REMOTE_PATH"; then
+if sshpass -p "$SSH_PASSWORD" scp -r -P "$SSH_PORT" -o StrictHostKeyChecking=no "$LOCAL_PATH" "$SSH_USER@$SSH_HOST:$REMOTE_PATH"; then
   echo "✅ Deployment finished successfully."
 else
   status=$?
