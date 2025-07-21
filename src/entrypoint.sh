344ad0d72b71624c010e45969953f1b10844d056
Alexandru Gojinetchi <MrFail@users.noreply.github.com>
2025-07-22 01:29:59 +0300

    feat(structure): Created alpha version

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+sshpass -p "$SSH_PASSWORD" ssh -p "$SSH_PORT" -o StrictHostKeyChecking=no user@remotehost "rm -r '$REMOTE_PATH'"
+
 # Run rsync and capture exit code
 if sshpass -p "$SSH_PASSWORD" scp -r -P "$SSH_PORT" -o StrictHostKeyChecking=no "$LOCAL_PATH" "$SSH_USER@$SSH_HOST:$REMOTE_PATH"; then
   echo "✅ Deployment finished successfully."
