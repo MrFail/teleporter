0a59c6976a95c7e27b2defafd9f4ab0ae047c534
Alexandru Gojinetchi <MrFail@users.noreply.github.com>
2025-07-22 01:45:21 +0300

    feat(structure): Created alpha version

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-sshpass -p "$SSH_PASSWORD" ssh -p "$SSH_PORT" -o StrictHostKeyChecking=no user@remotehost "rm -r '$REMOTE_PATH'"
+sshpass -p "$SSH_PASSWORD" ssh -p "$SSH_PORT" -o StrictHostKeyChecking=no "$SSH_USER@$SSH_HOST" "rm -r '$REMOTE_PATH'"
 
 # Run rsync and capture exit code
 if sshpass -p "$SSH_PASSWORD" scp -r -P "$SSH_PORT" -o StrictHostKeyChecking=no "$LOCAL_PATH" "$SSH_USER@$SSH_HOST:$REMOTE_PATH"; then
